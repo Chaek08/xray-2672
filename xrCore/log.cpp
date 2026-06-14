5ecc94e436647b7a582a6aa3da913ce159cece0d
Chaek08 <chaek777zz@gmail.com>
2026-06-14 16:56:12 +0300

    Доходим до меню с ресами от 2588

--- xrCore/log.cpp
+++ xrCore/log.cpp
@@ -32,10 +32,11 @@ void AddOne				(const char *split)
 {
 	logCS.Enter			();
 
-#ifdef DEBUG
-	OutputDebugString	(split);
-	OutputDebugString	("\n");
-#endif
+	if (IsDebuggerPresent())
+	{
+		OutputDebugString(split);
+		OutputDebugString("\n");
+	}
 
 	LogFile.push_back	(shared_str(split));
 
