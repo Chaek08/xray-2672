58948ce0f25e7a2e4ed8a3e36be389c8d8e45bea
Chaek08 <chaek777zz@gmail.com>
2026-06-14 13:51:12 +0300

    xrNetServer собирается

--- xrNetServer/NET_Server.cpp
+++ xrNetServer/NET_Server.cpp
@@ -156,7 +156,7 @@ BOOL IPureServer::Connect(LPCSTR options)
 	if (strchr(options,'/'))		strcpy(session_options, strchr(options,'/')+1);
 	if (strstr(options, "psw="))
 	{
-		char* PSW = strstr(options, "psw=") + 4;
+		const char* PSW = strstr(options, "psw=") + 4;
 		if (strchr(PSW, '/')) 
 			strncpy(password_str, PSW, strchr(PSW, '/') - PSW);
 		else
@@ -164,7 +164,7 @@ BOOL IPureServer::Connect(LPCSTR options)
 	}
 	if (strstr(options, "maxplayers="))
 	{
-		char* sMaxPlayers = strstr(options, "maxplayers=") + 11;
+		const char* sMaxPlayers = strstr(options, "maxplayers=") + 11;
 		string64 tmpStr = "";
 		if (strchr(sMaxPlayers, '/')) 
 			strncpy(tmpStr, sMaxPlayers, strchr(sMaxPlayers, '/') - sMaxPlayers);
@@ -594,7 +594,7 @@ bool			IPureServer::DisconnectAddress	(char* Address)
 
 	if (!NumPlayers) return false;
 
-	for (it = 0; it<NumPlayers; it++)
+	for (u32 it = 0; it<NumPlayers; it++)
 	{
 		DisconnectClient(PlayersToDisconnect[it]);
 	};
